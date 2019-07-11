f91a3100a57c498c7bf0a633742bb5eab90a7eaa
betsy wambui <bethwambuimuniu@gmail.com>
2019-07-11 21:32:26 +0300

    update script

--- frontend_setup.sh
+++ frontend_setup.sh
@@ -25,19 +25,19 @@ sudo apt remove cmdtest
 sudo apt remove yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install yarn -y
 
 #Make sure node and yarn are installed 
 if [[ "$?" != 0 ]]; then
    echo "An error was encountered when installing yarn"
-   exit - 1
+   exit 1
 else 
  git clone https://github.com/betsybeth/brightevent-frontend-app.git
  BRIGHTEVENTFILE="brightevent-frontend-app"
  #check if brightevent-frontend-app exists and it is a directory
  if [[ ! -d $BRIGHTEVENTFILE ]] && [[ -e  $BRIGHTEVENTFILE ]]; then
     echo " brightevent-frontend-app does not exit "
-     exit - 1
+     exit 1
   else
     chmod 777 brightevent-frontend-app
     cd brightevent-frontend-app 
